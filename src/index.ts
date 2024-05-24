48bd2facd5774d7981d2a181f7ef6d89ff439da2
rvincoy <vin23002@byui.edu>
2024-05-23 22:21:11 -0500

    more configs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import express, {Express, Request, Response} from "express";
+import dotenv from "dotenv";
+
+const app:Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/jobs', (req: Request, res: Response) => {
+    res.json({msg: 'Hello get jobs'});
+});
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
