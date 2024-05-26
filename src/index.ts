db652365f3cda1445482f47bf2210f8f855558cf
rvincoy <vin23002@byui.edu>
2024-05-26 10:35:38 -0500

    api v1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
 import express, {Express, Request, Response} from "express";
 import dotenv from "dotenv";
+import { getXataClient, Myproduct } from "./xata";
+
+dotenv.config();
 
 const app:Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
+const xata = getXataClient();
+app.use(express.json());
 
-app.get('/api/jobs', (req: Request, res: Response) => {
-    res.json({msg: 'Hello get jobs'});
+app.get('/api/products', async (req: Request, res: Response) => {
+    const products = await xata.db.myproduct.getAll();
+    res.json(products);
+});
+
+app.post('/api/products', async (req: Request, res: Response) => {
+    const product = req.body;
+    const newProduct = await xata.db.myproduct.create(product);
+    res.json(newProduct);
+});
+
+app.put('/api/products/:id', async (req: Request, res: Response) => {
+    const product = req.body;
+    const id = req.params.id;
+    const updateProduct = await xata.db.myproduct.update(id,product);
+    res.json(updateProduct);
+});
+
+app.delete('/api/products/:id', async (req: Request, res: Response) => {
+    const product = req.body;
+    const id = req.params.id;
+    const deleteProduct = await xata.db.myproduct.delete(id);
+    res.json(deleteProduct);
 });
 
 app.listen(port, () => {
