795bd8556a64f35cb1f1afe27f02b500a7492116
rvincoy <vin23002@byui.edu>
2024-05-26 14:56:41 -0500

    error handling

--- src/index.ts
+++ src/index.ts
@@ -10,28 +10,44 @@ const xata = getXataClient();
 app.use(express.json());
 
 app.get('/api/products', async (req: Request, res: Response) => {
-    const products = await xata.db.myproduct.getAll();
-    res.json(products);
+    try {
+        const products = await xata.db.myproduct.getAll();
+        res.json(products);       
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 app.post('/api/products', async (req: Request, res: Response) => {
-    const product = req.body;
-    const newProduct = await xata.db.myproduct.create(product);
-    res.json(newProduct);
+    try {
+        const product = req.body;
+        const newProduct = await xata.db.myproduct.create(product);
+        res.json(newProduct);      
+    } catch (error) {
+        console.error(error);      
+    }
 });
 
 app.put('/api/products/:id', async (req: Request, res: Response) => {
-    const product = req.body;
-    const id = req.params.id;
-    const updateProduct = await xata.db.myproduct.update(id,product);
-    res.json(updateProduct);
+    try {
+        const product = req.body;
+        const id = req.params.id;
+        const updateProduct = await xata.db.myproduct.update(id,product);
+        res.json(updateProduct);      
+    } catch (error) {
+        console.error(error);     
+    }
 });
 
 app.delete('/api/products/:id', async (req: Request, res: Response) => {
-    const product = req.body;
-    const id = req.params.id;
-    const deleteProduct = await xata.db.myproduct.delete(id);
-    res.json(deleteProduct);
+    try {
+        const product = req.body;
+        const id = req.params.id;
+        const deleteProduct = await xata.db.myproduct.delete(id);
+        res.json(deleteProduct);  
+    } catch (error) {
+        console.error(error);         
+    }
 });
 
 app.listen(port, () => {
